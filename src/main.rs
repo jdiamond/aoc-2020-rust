0c40c71d955ce378663b06b4a5088aa75c7c4e75
Jason Diamond <jason@diamond.name>
2020-12-02 15:14:41 -0800

    itertools

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::io::{self, BufRead};
 use std::str::FromStr;
 use std::vec::Vec;
@@ -13,17 +14,17 @@ fn main() {
 
     println!("got {} nums", nums.len());
 
-    for (index1, num1) in nums.iter().enumerate() {
-        for (index2, num2) in nums.iter().enumerate() {
-            if index1 < index2 {
-                let sum = num1 + num2;
+    let combos = nums.iter().combinations(2);
 
-                if sum == 2020 {
-                    let prod = num1 * num2;
+    for combo in combos {
+        let num1 = combo[0];
+        let num2 = combo[1];
+        let sum = num1 + num2;
 
-                    println!("{} * {} = {}", num1, num2, prod);
-                }
-            }
+        if sum == 2020 {
+            let prod = num1 * num2;
+
+            println!("{} * {} = {}", num1, num2, prod);
         }
     }
 }

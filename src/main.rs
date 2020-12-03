9a64633510ec62a79977dc125ce9c7d551d783ec
Jason Diamond <jason@diamond.name>
2020-12-02 18:31:52 -0800

    lookup table

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,21 @@ use std::env;
 
 mod day1;
 
+const FNS: [[fn(); 2]; 1] = [[day1::part1, day1::part2]];
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 3 {
         println!("usage: cargo run <day> <part> < input.txt");
     } else {
-        let day = &args[1];
-        let part = &args[2];
+        let day = &args[1].parse::<usize>().unwrap();
+        let part = &args[2].parse::<usize>().unwrap();
 
         println!("running day {} part {}", day, part);
 
-        if day == "1" && part == "1" {
-            day1::part1();
-        } else if day == "1" && part == "2" {
-            day1::part2();
-        }
+        let found = FNS[day - 1][part - 1];
+
+        found();
     }
 }

37cc0274ce1a8538ce0541c5064d0fe21a5cc97b
Jason Diamond <jason@diamond.name>
2020-12-02 16:10:17 -0800

    cli args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+use std::env;
+
 mod day1;
 
 fn main() {
-    // day1::part1();
-    day1::part2();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        println!("usage: cargo run <day> <part> < input.txt");
+    } else {
+        let day = &args[1];
+        let part = &args[2];
+
+        println!("running day {} part {}", day, part);
+
+        if day == "1" && part == "1" {
+            day1::part1();
+        } else if day == "1" && part == "2" {
+            day1::part2();
+        }
+    }
 }

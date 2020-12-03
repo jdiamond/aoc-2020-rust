0ab1709cf89de55e06aac49ff9c5593711c99c77
Jason Diamond <jason@diamond.name>
2020-12-02 17:37:39 -0800

    refactor to use sum and product

--- src/day1.rs
+++ src/day1.rs
@@ -4,32 +4,14 @@ use std::str::FromStr;
 use std::vec::Vec;
 
 pub fn part1() {
-    let stdin = io::stdin();
-    let mut nums = Vec::new();
-
-    for line in stdin.lock().lines() {
-        let num = u32::from_str(&line.unwrap()).unwrap();
-        nums.push(num);
-    }
-
-    println!("got {} nums", nums.len());
-
-    let combos = nums.iter().combinations(2);
-
-    for combo in combos {
-        let num1 = combo[0];
-        let num2 = combo[1];
-        let sum = num1 + num2;
-
-        if sum == 2020 {
-            let prod = num1 * num2;
-
-            println!("{} * {} = {}", num1, num2, prod);
-        }
-    }
+    run(2);
 }
 
 pub fn part2() {
+    run(3);
+}
+
+fn run(k: usize) {
     let stdin = io::stdin();
     let mut nums = Vec::new();
 
@@ -40,18 +22,14 @@ pub fn part2() {
 
     println!("got {} nums", nums.len());
 
-    let combos = nums.iter().combinations(3);
+    // must use into_iter instead of iter here or sum and product don't work
+    let combos = nums.into_iter().combinations(k);
 
     for combo in combos {
-        let num1 = combo[0];
-        let num2 = combo[1];
-        let num3 = combo[2];
-        let sum = num1 + num2 + num3;
+        let sum = combo.iter().sum::<u32>();
 
         if sum == 2020 {
-            let prod = num1 * num2 * num3;
-
-            println!("{} * {} * {} = {}", num1, num2, num3, prod);
+            println!("{:?}: {}", combo, combo.iter().product::<u32>());
         }
     }
 }

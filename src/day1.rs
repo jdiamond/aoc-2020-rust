8746991102085bbf101293725ac0349eb9e3cbdb
Jason Diamond <jason@diamond.name>
2020-12-02 16:02:23 -0800

    part2

--- src/day1.rs
+++ src/day1.rs
@@ -28,3 +28,30 @@ pub fn part1() {
         }
     }
 }
+
+pub fn part2() {
+    let stdin = io::stdin();
+    let mut nums = Vec::new();
+
+    for line in stdin.lock().lines() {
+        let num = u32::from_str(&line.unwrap()).unwrap();
+        nums.push(num);
+    }
+
+    println!("got {} nums", nums.len());
+
+    let combos = nums.iter().combinations(3);
+
+    for combo in combos {
+        let num1 = combo[0];
+        let num2 = combo[1];
+        let num3 = combo[2];
+        let sum = num1 + num2 + num3;
+
+        if sum == 2020 {
+            let prod = num1 * num2 * num3;
+
+            println!("{} * {} * {} = {}", num1, num2, num3, prod);
+        }
+    }
+}

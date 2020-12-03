a5d3d8e86c9bc0cf7b9b2d81f8aa50250f84f3c4
Jason Diamond <jason@diamond.name>
2020-12-02 21:20:44 -0800

    day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -2,6 +2,16 @@ use regex::Regex;
 use std::io::{self, BufRead};
 
 pub fn part1() {
+    run(parse_line1);
+}
+
+pub fn part2() {
+    run(parse_line2);
+}
+
+type LineParser = fn(line: &str) -> (Box<dyn PasswordPolicy>, String);
+
+fn run(parse_line: LineParser) {
     let stdin = io::stdin();
 
     let records = stdin.lock().lines().map(|line| parse_line(&line.unwrap()));
@@ -24,14 +34,18 @@ pub fn part1() {
     println!("there are {} valid passwords out of {} total", valid, total,)
 }
 
+trait PasswordPolicy {
+    fn validate(&self, password: &str) -> bool;
+}
+
 #[derive(Debug)]
-struct PasswordPolicy {
+struct PasswordPolicy1 {
     lowest: usize,
     highest: usize,
     letter: char,
 }
 
-impl PasswordPolicy {
+impl PasswordPolicy for PasswordPolicy1 {
     fn validate(&self, password: &str) -> bool {
         let count = password.chars().fold(0, |count, char| {
             if char == self.letter {
@@ -45,7 +59,7 @@ impl PasswordPolicy {
     }
 }
 
-fn parse_line(line: &str) -> (PasswordPolicy, String) {
+fn parse_line1(line: &str) -> (Box<dyn PasswordPolicy>, String) {
     // println!("{}", line);
 
     let re = Regex::new(r"(\d+)-(\d+) (.): (.+)").unwrap();
@@ -54,13 +68,46 @@ fn parse_line(line: &str) -> (PasswordPolicy, String) {
     // println!("{:?}", caps);
 
     (
-        PasswordPolicy {
+        Box::new(PasswordPolicy1 {
             lowest: caps.get(1).unwrap().as_str().parse::<usize>().unwrap(),
             highest: caps.get(2).unwrap().as_str().parse::<usize>().unwrap(),
             letter: caps.get(3).unwrap().as_str().chars().nth(0).unwrap(),
-        },
+        }),
         caps.get(4).unwrap().as_str().to_string(),
     )
 }
 
-pub fn part2() {}
+#[derive(Debug)]
+struct PasswordPolicy2 {
+    first: usize,
+    second: usize,
+    letter: char,
+}
+
+impl PasswordPolicy for PasswordPolicy2 {
+    fn validate(&self, password: &str) -> bool {
+        let first = password.chars().nth(self.first - 1).unwrap();
+        let second = password.chars().nth(self.second - 1).unwrap();
+
+        (first == self.letter && second != self.letter)
+            || (first != self.letter && second == self.letter)
+    }
+}
+
+fn parse_line2(line: &str) -> (Box<dyn PasswordPolicy>, String) {
+    // println!("{}", line);
+
+    let re = Regex::new(r"(\d+)-(\d+) (.): (.+)").unwrap();
+    let caps = re.captures(line).unwrap();
+
+    // println!("{:?}", caps);
+
+    (
+        Box::new(PasswordPolicy2 {
+            first: caps.get(1).unwrap().as_str().parse::<usize>().unwrap(),
+            second: caps.get(2).unwrap().as_str().parse::<usize>().unwrap(),
+            letter: caps.get(3).unwrap().as_str().chars().nth(0).unwrap(),
+        }),
+        caps.get(4).unwrap().as_str().to_string(),
+    )
+}

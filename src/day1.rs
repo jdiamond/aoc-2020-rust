95bc811eb223ac82887513bb4086b60fa98909ad
Jason Diamond <jason@diamond.name>
2020-12-02 18:10:19 -0800

    chain iterators

--- src/day1.rs
+++ src/day1.rs
@@ -1,7 +1,6 @@
 use itertools::Itertools;
 use std::io::{self, BufRead};
 use std::str::FromStr;
-use std::vec::Vec;
 
 pub fn part1() {
     run(2);
@@ -13,23 +12,15 @@ pub fn part2() {
 
 fn run(k: usize) {
     let stdin = io::stdin();
-    let mut nums = Vec::new();
 
-    for line in stdin.lock().lines() {
-        let num = u32::from_str(&line.unwrap()).unwrap();
-        nums.push(num);
-    }
+    let results = stdin
+        .lock()
+        .lines()
+        .map(|line| u32::from_str(&line.unwrap()).unwrap())
+        .combinations(k)
+        .filter(|combo| combo.iter().sum::<u32>() == 2020);
 
-    println!("got {} nums", nums.len());
-
-    // must use into_iter instead of iter here or sum and product don't work
-    let combos = nums.into_iter().combinations(k);
-
-    for combo in combos {
-        let sum = combo.iter().sum::<u32>();
-
-        if sum == 2020 {
-            println!("{:?}: {}", combo, combo.iter().product::<u32>());
-        }
+    for result in results {
+        println!("{:?}: {}", result, result.iter().product::<u32>());
     }
 }

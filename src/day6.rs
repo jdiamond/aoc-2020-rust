46ab6b6405fc504fefd82a635b9c8ec24b359690
Jason Diamond <jason@diamond.name>
2020-12-05 21:21:44 -0800

    day 6 part 1

--- src/day6.rs
+++ src/day6.rs
@@ -1,5 +1,21 @@
-pub fn part1(_input: &str) -> String {
-    String::from("")
+use std::collections::HashSet;
+
+pub fn part1(input: &str) -> String {
+    let votes = input
+        .split("\n\n")
+        .map(|group| {
+            group
+                .lines()
+                .flat_map(|line| line.chars())
+                .collect::<HashSet<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    // dbg!(&votes);
+
+    let votes = votes.iter().fold(0, |sum, group| sum + group.len());
+
+    String::from(votes.to_string())
 }
 
 pub fn part2(_input: &str) -> String {
@@ -15,14 +31,14 @@ mod tests {
     fn part1_example_works() {
         let input = load_input("day6-example.txt");
 
-        assert_eq!(part1(&input), "");
+        assert_eq!(part1(&input), "11");
     }
 
     #[test]
     fn part1_input_works() {
         let input = load_input("day6-puzzle.txt");
 
-        assert_eq!(part1(&input), "");
+        assert_eq!(part1(&input), "6532");
     }
 
     #[test]

78ba4d708586c8eeec750ee2b732218e9ec63176
Jason Diamond <jason@diamond.name>
2020-12-05 21:39:55 -0800

    day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -18,8 +18,26 @@ pub fn part1(input: &str) -> String {
     String::from(votes.to_string())
 }
 
-pub fn part2(_input: &str) -> String {
-    String::from("")
+pub fn part2(input: &str) -> String {
+    let votes = input
+        .split("\n\n")
+        .map(|group| {
+            let all = String::from("abcdefghijklmnopqrstuvwxzy")
+                .chars()
+                .collect::<HashSet<_>>();
+
+            group
+                .lines()
+                .map(|line| line.chars().collect::<HashSet<_>>())
+                .fold(all, |acc, cur| acc.intersection(&cur).map(|c| *c).collect())
+        })
+        .collect::<Vec<_>>();
+
+    // dbg!(&votes);
+
+    let votes = votes.iter().fold(0, |sum, group| sum + group.len());
+
+    String::from(votes.to_string())
 }
 
 #[cfg(test)]
@@ -45,13 +63,13 @@ mod tests {
     fn part2_example_works() {
         let input = load_input("day6-example.txt");
 
-        assert_eq!(part2(&input), "");
+        assert_eq!(part2(&input), "6");
     }
 
     #[test]
     fn part2_input_works() {
         let input = load_input("day6-puzzle.txt");
 
-        assert_eq!(part2(&input), "");
+        assert_eq!(part2(&input), "3427");
     }
 }

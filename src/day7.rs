5cf6819b29b31e0d078ad685e98e0ec2e54b3cb7
Jason Diamond <jason@diamond.name>
2020-12-07 15:58:53 -0800

    day 7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -14,36 +14,7 @@ struct Content {
 }
 
 pub fn part1(input: &str) -> String {
-    let re = Regex::new(r"(\w+ \w+) bags contain (.+)\.").unwrap();
-    let re2 = Regex::new(r"(\d+) (\w+ \w+) bags?").unwrap();
-
-    let rules = input
-        .lines()
-        .map(|line| re.captures(line).unwrap())
-        .map(|cap| {
-            let contents = cap.get(2).unwrap().as_str().to_owned();
-            let contents = if contents == "no other bags" {
-                vec![]
-            } else {
-                contents
-                    .split(", ")
-                    .map(|s| {
-                        let cap = re2.captures(s).unwrap();
-
-                        Content {
-                            qty: cap.get(1).unwrap().as_str().parse::<usize>().unwrap(),
-                            color: cap.get(2).unwrap().as_str().to_owned(),
-                        }
-                    })
-                    .collect::<Vec<_>>()
-            };
-
-            Rule {
-                color: cap.get(1).unwrap().as_str().to_owned(),
-                contents: contents,
-            }
-        })
-        .collect::<Vec<_>>();
+    let rules = parse(input);
 
     // dbg!(&rules);
 
@@ -76,8 +47,64 @@ fn bag_contains(map: &HashMap<String, &Rule>, outer: &str, inner: &str) -> bool 
     false
 }
 
-pub fn part2(_input: &str) -> String {
-    String::from("")
+pub fn part2(input: &str) -> String {
+    let rules = parse(input);
+
+    // dbg!(&rules);
+
+    let map = rules
+        .iter()
+        .map(|rule| (rule.color.clone(), rule))
+        .collect::<HashMap<_, _>>();
+
+    // dbg!(&map);
+
+    String::from((count_inners(&map, "shiny gold") - 1).to_string())
+}
+
+fn count_inners(map: &HashMap<String, &Rule>, outer: &str) -> usize {
+    let outer = map[outer];
+
+    1 + outer
+        .contents
+        .iter()
+        .map(|c| c.qty * count_inners(map, &c.color))
+        .sum::<usize>()
+}
+
+fn parse(input: &str) -> Vec<Rule> {
+    let re = Regex::new(r"(\w+ \w+) bags contain (.+)\.").unwrap();
+    let re2 = Regex::new(r"(\d+) (\w+ \w+) bags?").unwrap();
+
+    let rules = input
+        .lines()
+        .map(|line| re.captures(line).unwrap())
+        .map(|cap| {
+            let contents = cap.get(2).unwrap().as_str().to_owned();
+            let contents = if contents == "no other bags" {
+                vec![]
+            } else {
+                contents
+                    .split(", ")
+                    .map(|s| {
+                        let cap = re2.captures(s).unwrap();
+
+                        Content {
+                            qty: cap.get(1).unwrap().as_str().parse::<usize>().unwrap(),
+                            color: cap.get(2).unwrap().as_str().to_owned(),
+                        }
+                    })
+                    .collect::<Vec<_>>()
+            };
+
+            Rule {
+                color: cap.get(1).unwrap().as_str().to_owned(),
+                contents: contents,
+            }
+        })
+        .collect::<Vec<_>>();
+
+    rules
 }
 
 #[cfg(test)]
@@ -101,15 +128,15 @@ mod tests {
 
     #[test]
     fn part2_example_works() {
-        let input = load_input("day7-example.txt");
+        let input = load_input("day7-example2.txt");
 
-        assert_eq!(part2(&input), "");
+        assert_eq!(part2(&input), "126");
     }
 
     #[test]
     fn part2_input_works() {
         let input = load_input("day7-puzzle.txt");
 
-        assert_eq!(part2(&input), "");
+        assert_eq!(part2(&input), "38426");
     }
 }

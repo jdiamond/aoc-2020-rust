57ec228b3d2c0de5778ff2d52925f8d4c249e195
Jason Diamond <jason@diamond.name>
2020-12-09 19:13:36 -0800

    part 2

--- src/day9.rs
+++ src/day9.rs
@@ -1,9 +1,45 @@
 use itertools::Itertools;
+use itertools::MinMaxResult::MinMax;
 
 pub fn part1(input: &str) -> String {
     let nums = parse(input);
-
     let size = nums[0];
+    let first_invalid = find_first_invalid(&nums, size);
+
+    first_invalid.to_string()
+}
+
+pub fn part2(input: &str) -> String {
+    let nums = parse(input);
+    let size = nums[0];
+    let first_invalid = find_first_invalid(&nums, size);
+    let weakness = find_weakness(&nums, first_invalid);
+
+    weakness.to_string()
+}
+
+fn find_weakness(nums: &Vec<usize>, sum: usize) -> usize {
+    for (start, num) in nums.iter().enumerate() {
+        let mut x = *num;
+
+        for (end, next) in nums.iter().enumerate().skip(start + 1) {
+            x += *next;
+
+            if x == sum {
+                return match nums[start..end + 1].iter().minmax() {
+                    MinMax(min, max) => min + max,
+                    _ => 0,
+                };
+            } else if x > sum {
+                break;
+            }
+        }
+    }
+
+    0
+}
+
+fn find_first_invalid(nums: &Vec<usize>, size: usize) -> usize {
     let mut index = size + 1;
 
     loop {
@@ -14,7 +50,7 @@ pub fn part1(input: &str) -> String {
         index += 1;
     }
 
-    nums[index].to_string()
+    nums[index]
 }
 
 fn is_valid(nums: &Vec<usize>, size: usize, index: usize) -> bool {
@@ -24,10 +60,6 @@ fn is_valid(nums: &Vec<usize>, size: usize, index: usize) -> bool {
         .iter()
         .combinations(2)
         .any(|combo| combo[0] + combo[1] == current)
-}
-
-pub fn part2(_input: &str) -> String {
-    String::from("")
 }
 
 fn parse(input: &str) -> Vec<usize> {
@@ -60,13 +92,13 @@ mod tests {
     fn part2_example_works() {
         let input = load_input("day9-example.txt");
 
-        assert_eq!(part2(&input), "");
+        assert_eq!(part2(&input), "62");
     }
 
     #[test]
     fn part2_input_works() {
         let input = load_input("day9-puzzle.txt");
 
-        assert_eq!(part2(&input), "");
+        assert_eq!(part2(&input), "219202240");
     }
 }
